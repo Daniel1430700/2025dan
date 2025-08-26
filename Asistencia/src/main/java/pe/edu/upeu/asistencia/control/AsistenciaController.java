ba9b5cf8a0cfa6b6e0c0eb4f2992588c24ac510a
DanielSistE <danieleddyc7@gmail.com>
2025-08-26 13:03:51 -0500

    proyectactualizado1

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
@@ -1,21 +1,26 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import pe.edu.upeu.asistencia.modelo.Estudiante;
+import pe.edu.upeu.asistencia.servicio.EstudianteServicioI;
 
 @Controller
 public class AsistenciaController {
+
+    @Autowired
+     EstudianteServicioI estudianteServicioI;
+
     @FXML
     private Label idMsg;
     @FXML
-    private Label idMsg1;
-    @FXML
     TextField txtDato;
-    @FXML
-    TextField txtDato1;
 
     //@FXML Button btnEnviar;
 
@@ -23,6 +28,21 @@ public class AsistenciaController {
     void enviar(){
         System.out.println("Enviando asistencia");
         idMsg.setText(txtDato.getText());
-        idMsg1.setText(txtDato1.getText());
+
+    }
+    @FXML
+    public void regEstudiante(){
+        Estudiante estudiante = new Estudiante();
+        estudiante.setNombre(new SimpleStringProperty(txtDato.getText()));
+        estudiante.setEstado(new SimpleBooleanProperty());
+
+        estudianteServicioI.saveEntidad(estudiante);
+        listaEstudiantes();
+
+    }
+    void listaEstudiantes(){
+        for (Estudiante e: estudianteServicioI.findAllEntidades()){
+            System.out.println(e.getNombre());
+        }
     }
 }
