97a79b8cd77f73890633dac67f068f95c66168b9
DanielSistE <danieleddyc7@gmail.com>
2025-10-07 09:30:38 -0500

    WEB-RECURSOS

---
+++ WEB-RECURSOS/WEB-RECURSOS/java/AsistenciaApplicationw.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.asistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaApplicationw {
+    public static void main(String[] args) {
+        SpringApplication.run(AsistenciaApplicationw.class, args);
+    }
+}
